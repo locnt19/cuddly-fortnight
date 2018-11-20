19d8c23a3415886d61e0ab765e0efdf46cebc3fc
Nguyễn Thành Lộc <locnt19@gmail.com>
2018-11-21 02:08:40 +0700

    init project

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+// 1.home-banner
+$(document).ready(function () {
+	$('.banner .owl-carousel').owlCarousel({
+		nav: true,
+		dots: false,
+		items: 1,
+		loop: true,
+		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>']
+	});
+});
