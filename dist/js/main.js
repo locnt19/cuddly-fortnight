913b267f52fede10d6d933b69ccabd6d12d9bae6
Nguyễn Thành Lộc <locnt19@gmail.com>
2018-11-30 18:19:21 +0700

    complete

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,7 +31,6 @@ $(function () {
 		$('select.form-control').removeClass('form-control').css('width', '100%');
 	}
 });
-// 1.home-banner
 $(document).ready(function () {
 	$('.banner .owl-carousel').owlCarousel({
 		nav: true,
@@ -44,9 +43,13 @@ $(document).ready(function () {
 	$('.orient .data-tabs .owl-carousel').owlCarousel({
 		nav: false,
 		dots: false,
-		items: 3,
-		loop: true
-		// navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>']
+		items: 1,
+		loop: true,
+		responsive: {
+			768: {
+				items: 3
+			}
+		}
 	});
 
 	$('.review .avatar .owl-carousel').owlCarousel({
@@ -62,28 +65,31 @@ $(document).ready(function () {
 			}
 		}
 	});
+
+	$('.partner .owl-carousel').owlCarousel({
+		nav: true,
+		dots: false,
+		items: 2,
+		loop: true,
+		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+		responsive: {
+			768: {
+				items: 5
+			}
+		}
+	});
 });
 
 function filter(e) {
-	var dung = 0;
-	var sai = 0;
-	$(this).parents('.tabs').find('li').toggleClass('active');
-	// $('.orient .data-tabs .owl-item .item').each(function () {
-	// 	$(this).parents('.owl-item').show()
-	// 	if($(this).hasClass(e) == true) {
-	// 		$(this).parents('.owl-item').show()
-	// 		dung++
-	// 	}
-	// 	else {
-	// 		$(this).parents('.owl-item').hide()
-	// 		sai++
-	// 	}
-	// })
-	console.log('dung = ', dung);
-	console.log('sai = ', sai);
+	$('.orient .data-tabs .owl-carousel .item').show();
+	$('.orient .data-tabs .owl-carousel .item').each(function () {
+		if ($(this).hasClass(e) == true) {
+			$(this).show();
+			dung++;
+			console.log('dung');
+		} else {
+			$(this).hide();
+		}
+	});
 }
-
-// $('.tabs ul li a').on('click', function(){
-// 	$(this).parents('.tabs').find('li').toggleClass('active')
-// })
 //# sourceMappingURL=main.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,3 @@
-// 1.home-banner
 $(document).ready(function () {
 	$('.banner .owl-carousel').owlCarousel({
 		nav: true,
@@ -11,9 +10,13 @@ $(document).ready(function () {
 	$('.orient .data-tabs .owl-carousel').owlCarousel({
 		nav: false,
 		dots: false,
-		items: 3,
-		loop: true
-		// navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>']
+		items: 1,
+		loop: true,
+		responsive: {
+			768: {
+				items: 3
+			}
+		}
 	});
 
 	$('.review .avatar .owl-carousel').owlCarousel({
@@ -30,27 +33,32 @@ $(document).ready(function () {
 		}
 	});
 
+	$('.partner .owl-carousel').owlCarousel({
+		nav: true,
+		dots: false,
+		items: 2,
+		loop: true,
+		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+		responsive: {
+			768: {
+				items: 5
+			}
+		}
+	});
+
+
 });
 
 function filter(e) {
-	let dung  = 0;
-	let sai  = 0;
-	$(this).parents('.tabs').find('li').toggleClass('active')
-	// $('.orient .data-tabs .owl-item .item').each(function () {
-	// 	$(this).parents('.owl-item').show()
-	// 	if($(this).hasClass(e) == true) {
-	// 		$(this).parents('.owl-item').show()
-	// 		dung++
-	// 	}
-	// 	else {
-	// 		$(this).parents('.owl-item').hide()
-	// 		sai++
-	// 	}
-	// })
-	console.log('dung = ', dung)
-	console.log('sai = ', sai)
+	$('.orient .data-tabs .owl-carousel .item').show();
+	$('.orient .data-tabs .owl-carousel .item').each(function () {
+		if($(this).hasClass(e) == true) {
+			$(this).show();
+			dung++;
+			console.log('dung');
+		}
+		else {
+			$(this).hide();
+		}
+	})
 }
-
-// $('.tabs ul li a').on('click', function(){
-// 	$(this).parents('.tabs').find('li').toggleClass('active')
-// })
